a80a3f4a2b458e4e2c9fcccf1058ee30d6ec8e01
Abdul Wahab Butt <102326149+00awb00@users.noreply.github.com>
2022-11-21 11:11:43 +0500

    Update settings.py

--- Drosometer/settings.py
+++ Drosometer/settings.py
@@ -136,11 +136,3 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
